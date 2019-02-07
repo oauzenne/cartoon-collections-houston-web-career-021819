b50831ccedeae2023bc68ab47f4529d0e70d746b
Olivia Auzenne <oauzenne@gmail.com>
2019-02-07 18:01:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,3 +4,19 @@ def roll_call_dwarves(array)
   }
 end
 
+def summon_captain_planet(array)
+  array.map do |phrase|
+    phrase.capitalize+"!"
+  end
+end
+
+def long_planeteer_calls(array)
+ array.any? {|call|
+    call.length > 4
+  }
+end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|type| type == cheese_types[0] ||  type == cheese_types[1] ||  type == cheese_types[2]}
+end	
